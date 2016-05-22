b66e2587bd536896786f5538765d6358e5b74a6d
rasingard <rasingard@gmail.com>
2016-05-22 06:07:29 +0000

    UI update - JSON get

--- ui/script/scripts.js
+++ ui/script/scripts.js
@@ -88,3 +88,141 @@ s.startForceAtlas2();
 
 //console.log(JSON.stringify(g));
 */
+
+
+
+
+
+
+/*
+        var sigmaGraph = sigma.parsers.json('data.json', {
+            renderer: {
+                container: document.getElementById('container'),
+                type: 'canvas'
+            },
+            settings: {
+                edgeLabelSize: 'proportional',
+                minEdgeSize: 1,
+                maxEdgeSize: 11,
+                minNodeSize: 14,
+                minNodeSize: 14,
+                defaultLabelColor: '#fff',
+                borderSize: 3,
+                defaultNodeBorderColor: '#fff'
+            }
+        }, function(s) {
+            
+            $('#filter').autocomplete({
+                source: s.graph.nodes(),
+                select: function( event, ui ) {
+                    var nodeId = ui.item.id,
+                        toKeep = s.graph.neighbors(nodeId);
+                    toKeep[nodeId] = ui.item;
+            
+                    s.graph.nodes().forEach(function(n) {
+                        if (toKeep[n.id])
+                            n.color = n.originalColor;
+                        else
+                            n.color = '#3a3e52';
+                    });
+            
+                    s.graph.edges().forEach(function(e) {
+                        if (toKeep[e.source] && toKeep[e.target])
+                            e.color = e.originalColor;
+                        else
+                            e.color = '#252a40';
+                    });
+                    
+                    var position = s.graph.goToNodes(toKeep, s.camera);
+                    s.camera.goTo({
+                        x: position.centerX,
+                        y: position.centerY
+                    });
+                    
+                    // Since the data has been modified, we need to
+                    // call the refresh method to make the colors
+                    // update effective.
+                    s.refresh();
+                }
+            });
+            
+            var startForceRunning = false;
+            $('#orderNodes').button();
+            $('#orderNodes').on('click', function(event, ui) {
+                if(!startForceRunning) {
+                    $('#orderNodes').button('option', 'label', 'Order Nodes');
+                    startForceRunning = true;
+                    s.startForceAtlas2();
+                } else {
+                    $('#orderNodes').button('option', 'label', 'stop Ordering');
+                    startForceRunning = false;
+                    s.stopForceAtlas2();
+                }
+            });
+        
+            var rangeVal = getRange(s);
+            drawRange(rangeVal.max, rangeVal.min);
+        
+            // We first need to save the original colors of our
+            // nodes and edges, like this:
+            s.graph.nodes().forEach(function(n) {
+                n.originalColor = n.color;
+            });
+        
+            s.graph.edges().forEach(function(e) {
+                e.originalColor = e.color;
+            });
+        
+            // When a node is clicked, we check for each node
+            // if it is a neighbor of the clicked one. If not,
+            // we set its color as grey, and else, it takes its
+            // original color.
+            // We do the same for the edges, and we only keep
+            // edges that have both extremities colored.
+            s.bind('clickNode', function(e) {
+                var nodeId = e.data.node.id,
+                    toKeep = s.graph.neighbors(nodeId);
+                toKeep[nodeId] = e.data.node;
+        
+                s.graph.nodes().forEach(function(n) {
+                    if (toKeep[n.id])
+                        n.color = n.originalColor;
+                    else
+                        n.color = '#3a3e52';
+                });
+        
+                s.graph.edges().forEach(function(e) {
+                    if (toKeep[e.source] && toKeep[e.target])
+                        e.color = e.originalColor;
+                    else
+                        e.color = '#252a40';
+                });
+                
+                var position = s.graph.goToNodes(toKeep, s.camera);
+                s.camera.goTo({
+                    x: position.centerX,
+                    y: position.centerY
+                });
+                
+                // Since the data has been modified, we need to
+                // call the refresh method to make the colors
+                // update effective.
+                s.refresh();
+            });
+        
+            // When the stage is clicked, we just color each
+            // node and edge with its original color.
+            s.bind('clickStage', function(e) {
+                s.graph.nodes().forEach(function(n) {
+                    n.color = n.originalColor;
+                });
+        
+                s.graph.edges().forEach(function(e) {
+                    e.color = e.originalColor;
+                });
+        
+                // Same as in the previous event:
+                s.refresh();
+            });
+        });
+        */
