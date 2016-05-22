f83a439ee9fb2781b156e6a633774f4df412b043
maviteixeira <marcoo.teixeira@gmail.com>
2016-05-22 00:21:26 -0300

    New postman collection.

--- test/serviceCall/test.js
+++ test/serviceCall/test.js
@@ -8,10 +8,5 @@
     let data = { "bytes": "281", "port": "9020", "origin": "127.0.0.1", "destination": "127.0.0.1", "request_uri": "/m2/", "time": "2016-05-21T15:38:08-03:00", "status_code": "200", "request_time": "0.035", "method": "GET" };
 
     service.findAggAll();
-    
-    
-    
-    
-    
 
 })();
