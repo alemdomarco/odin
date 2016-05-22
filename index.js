eb6bb835a997969804044d08975c0ee04bdacabf
maviteixeira <marcoo.teixeira@gmail.com>
2016-05-22 01:30:33 -0300

    Temp disabled some things :)

--- index.js
+++ index.js
@@ -5,8 +5,9 @@
   const OdinFileProducer = require("./producer/odin-file-producer");
   const Server = require('./server.js');
   
-  let odinFileProducer = new OdinFileProducer();
-  odinFileProducer.produceOdinDate();
+  //let odinFileProducer = new OdinFileProducer();
+  // Temp disabled
+  //odinFileProducer.produceOdinDate();
   
   let server = new Server();
 	server.startServer();
